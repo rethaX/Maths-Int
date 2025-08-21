837aa1bfffbbd4cb1687e033a74855242c76f8a7
Rethabile Mokwane <75128828+rethaX@users.noreply.github.com>
2025-08-21 14:11:40 +0200

    updated equation with solving code

--- src/main/java/org/maths/Main.java
+++ src/main/java/org/maths/Main.java
@@ -3,10 +3,13 @@ package org.maths;
 public class Main {
     public static void main(String[] args) {
 
-        String a1 = "10";
-        int i = Integer.parseInt(a1);
+        //(a+=b)^2 = a^2 + 2ab + b^2
 
-        System.out.print(i);
+        int a = 25;
+        float b = 42.159f;
+        double value = a*a + 2*(a*b)+ b*b;
+
+        System.out.print((float)value);
 
     }
 }
