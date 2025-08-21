44cb50244cf5d2e000d7ae87ddaef87c2029c5e4
Rethabile Mokwane <75128828+rethaX@users.noreply.github.com>
2025-08-21 13:59:10 +0200

    Equation template commit

---
+++ src/main/java/org/maths/Main.java
@@ -0,0 +1,12 @@
+package org.maths;
+
+public class Main {
+    public static void main(String[] args) {
+
+        String a1 = "10";
+        int i = Integer.parseInt(a1);
+
+        System.out.print(i);
+
+    }
+}
